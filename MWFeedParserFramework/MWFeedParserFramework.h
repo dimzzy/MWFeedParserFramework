67fdf22da194e598bb7f573aa691ad514dcb3e47
Dmitry Stadnik <dimzzy@gmail.com>
2011-12-28 23:01:30 +0100

    Initial Commit

---
+++ MWFeedParserFramework/MWFeedParserFramework.h
@@ -0,0 +1,13 @@
+//
+//  MWFeedParserFramework.h
+//  MWFeedParserFramework
+//
+//  Created by Stadnik Dmitry on 12/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MWFeedParserFramework : NSObject
+
+@end
---
+++ MWFeedParserFramework/MWFeedParserFramework.m
@@ -0,0 +1,23 @@
+//
+//  MWFeedParserFramework.m
+//  MWFeedParserFramework
+//
+//  Created by Stadnik Dmitry on 12/28/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MWFeedParserFramework.h"
+
+@implementation MWFeedParserFramework
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
